eef6655fb4f7e23dc6d4e7ac8cca99fa9355a74c
sayar1n <zalexhirai@gmail.com>
2024-10-01 16:01:04 +0300

    Форма восстановления пароля

--- frontend/src/app/passReset/page.tsx
+++ frontend/src/app/passReset/page.tsx
@@ -1,5 +1,28 @@
-export default function passReset(){
-    return (
-        <p>Лол</p>
+import Link from 'next/link';
+import styles from './page.module.css';
+
+export default function SignIn(){
+    return(
+        <div className={styles.SignInBlock}>
+            <div className={styles.logo}>Articly</div>
+            <div className={styles.welcomeText}>
+                {/* <p className={styles.boldHello}>С возвращением!</p> */}
+                <p>Пожалуйста, введите Ваш новый пароль <br/> Данный пароль будет использован для входа в сервис</p>
+            </div>
+            <div className={styles.inputField}>
+                <div className={styles.inputEmail}>
+                    <label className={styles.label}>Новый пароль:</label>
+                    <input type="password" className={styles.emailInputBox} placeholder="********" id="password"/>
+                </div>
+                <div className={styles.inputPassword}>
+                    <label className={styles.label}>Подтверждение:</label>
+                    <input type="password" className={styles.passwordInputBox} placeholder="********" id="password" />
+                </div>
+            </div>
+            <Link href="" className={styles.signInButton}>Изменить пароль</Link>
+            <div className={styles.signUpBox}>
+                <Link href="/signIn" className={styles.signUpText}>Войти в аккаунт</Link>
+            </div>
+        </div>
     )
 }
--- frontend/src/app/signIn/page.tsx
+++ frontend/src/app/signIn/page.tsx
@@ -1,6 +1,5 @@
 import Link from 'next/link';
 import styles from './page.module.css';
-import Header from '@/components/header/Header';
 
 export default function SignIn(){
     return(
