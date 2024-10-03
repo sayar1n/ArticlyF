cfd6fecbdb2898a9d950f6607380c403072a14d0
sayar1n <zalexhirai@gmail.com>
2024-10-03 22:24:37 +0300

    Error Page

---
+++ frontend/src/app/Calendar/page.tsx
@@ -0,0 +1,121 @@
+import styles from "./page.module.css"
+
+export default function Calendar() {
+    return (
+        <div className={styles.v6_3}>
+            <div className={styles.v63_51}>
+                <div className={styles.v174_681}></div>
+                <div className={styles.v63_54}><span className={styles.v63_55}>Сегодня</span></div>
+                <div className={styles.v160_127}><span className={styles.v160_128}>Задача</span>
+                    <div className={styles.v160_136}><span className={styles.v160_137}>+</span></div>
+                </div>
+                <div className={styles.v160_130}><span className={styles.v160_131}>Событие</span>
+                    <div className={styles.v160_138}><span className={styles.v160_139}>+</span></div>
+                </div><span className={styles.v63_56}>Сентябрь, 2024</span><span className={styles.v160_35}>Сентябрь, 2024</span>
+                <div className={styles.v63_58}>
+                    <div className={styles.v69_59}>
+                        <div className={styles.v69_60}></div>
+                    </div>
+                </div>
+                <div className={styles.v69_62}>
+                    <div className={styles.v69_63}>
+                        <div className={styles.v69_64}></div>
+                    </div>
+                </div>
+                <div className={styles.v160_45}>
+                    <div className={styles.v160_41}>
+                        <div className={styles.v160_42}>
+                            <div className={styles.v160_43}></div>
+                        </div>
+                    </div>
+                    <div className={styles.v160_37}>
+                        <div className={styles.v160_38}>
+                            <div className={styles.v160_39}></div>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.v69_84}>
+                    <div className={styles.v69_66}><span className={styles.v69_68}>День</span></div>
+                    <div className={styles.v69_69}><span className={styles.v69_70}>Неделя</span></div>
+                    <div className={styles.name}></div>
+                    <div className={styles.v69_73}><span className={styles.v69_74}>Месяц</span></div>
+                    <div className={styles.v69_76}><span className={styles.v69_77}>Год</span></div>
+                    <div className={styles.name}></div>
+                    <div className={styles.name}></div>
+                </div><span className={styles.v63_52}>Календарь</span>
+                <div className={styles.v160_124}>
+                    <div className={styles.v160_70}><span className={styles.v160_57}>1</span><span className={styles.v160_58}>31</span><span
+                        className={styles.v160_60}>30</span><span className={styles.v160_62}>29</span><span className={styles.v160_64}>28</span><span
+                            className={styles.v160_68}>26</span><span className={styles.v160_66}>27</span></div>
+                    <div className={styles.v160_79}><span className={styles.v160_80}>8</span><span className={styles.v160_81}>7</span><span
+                        className={styles.v160_82}>6</span><span className={styles.v160_83}>5</span><span className={styles.v160_84}>4</span><span
+                            className={styles.v160_85}>3</span><span className={styles.v160_86}>2</span></div>
+                    <div className={styles.v160_88}><span className={styles.v160_89}>15</span><span className={styles.v160_90}>14</span><span
+                        className={styles.v160_91}>13</span><span className={styles.v160_92}>12</span><span className={styles.v160_93}>11</span><span
+                            className={styles.v160_94}>10</span><span className={styles.v160_95}>9</span></div>
+                    <div className={styles.v160_97}><span className={styles.v160_98}>22</span><span className={styles.v160_99}>21</span><span
+                        className={styles.v160_100}>20</span><span className={styles.v160_101}>19</span><span className={styles.v160_102}>18</span><span
+                            className={styles.v160_103}>17</span><span className={styles.v160_104}>16</span></div>
+                    <div className={styles.v160_106}><span className={styles.v160_107}>29</span><span className={styles.v160_108}>28</span>
+                        <div className={styles.v174_685}></div><span className={styles.v160_109}>27</span><span className={styles.v160_110}>26</span><span
+                            className={styles.v160_111}>25</span><span className={styles.v160_112}>24</span><span className={styles.v160_113}>23</span>
+                    </div>
+                    <div className={styles.v160_115}><span className={styles.v160_116}>6</span><span className={styles.v160_117}>5</span><span
+                        className={styles.v160_118}>4</span><span className={styles.v160_119}>3</span><span className={styles.v160_120}>2</span><span
+                            className={styles.v160_121}>1</span><span className={styles.v160_122}>30</span></div>
+                </div><span className={styles.v160_141}>Грядущие события</span>
+                <div className={styles.name}></div>
+                <div className={styles.name}></div>
+                <div className={styles.v160_1246}><span className={styles.v160_1233}>Понедельник</span><span className={styles.v160_1234}>Вторник</span><span
+                    className={styles.v160_1236}>Среда</span><span className={styles.v160_1238}>Четверг</span><span
+                        className={styles.v160_1240}>Пятница</span><span className={styles.v160_1242}>Суббота</span><span
+                            className={styles.v160_1244}>Воскресенье</span></div><span className={styles.v160_1279}>26</span><span
+                                className={styles.v160_1280}>26</span><span className={styles.v160_1282}>26</span><span className={styles.v160_1284}>27</span><span
+                                    className={styles.v160_1286}>28</span><span className={styles.v160_1288}>29</span><span className={styles.v160_1290}>30</span><span
+                                        className={styles.v160_1292}>31</span><span className={styles.v160_1294}>1</span>
+                <div className={styles.v160_1296}><span className={styles.v160_1295}>Полить цветочки</span></div>
+                <div className={styles.v160_1297}><span className={styles.v160_1298}>Схадить пакушать</span></div>
+            </div>
+            <div className={styles.name}></div>
+            <div className={styles.name}></div>
+            <div className={styles.name}></div>
+            <div className={styles.name}></div>
+            <div className={styles.name}></div>
+            <div className={styles.name}></div>
+            <div className={styles.v63_25}>
+                <div className={styles.v174_689}></div><span className={styles.v63_14}>Articly</span>
+                <div className={styles.v69_85}>
+                    <div className={styles.v63_31}>
+                        <div className={styles.v63_26}>
+                            <div className={styles.v63_27}></div>
+                        </div><span className={styles.v63_30}>Календарь</span>
+                    </div>
+                    <div className={styles.v63_32}>
+                        <div className={styles.v63_33}>
+                            <div className={styles.v63_34}></div>
+                        </div>
+                        <span className={styles.v63_35}>Заметки</span>
+                    </div>
+                    <div className={styles.v63_37}><span className={styles.v63_40}>Задачи</span>
+                        <div className={styles.v63_45}>
+                            <div className={styles.v63_46}></div>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.v63_49}>
+                    <div className={styles.v61_11}>
+                        <div className={styles.v63_22}>
+                            <div className={styles.v63_23}></div>
+                        </div>
+                    </div><span className={styles.v63_48}>Настройки</span>
+                </div>
+            </div>
+            <div className={styles.name}></div>
+            <div className={styles.name}></div>
+            <div className={styles.name}></div>
+            <div className={styles.name}></div>
+            <div className={styles.name}></div>
+            <div className={styles.name}></div>
+        </div>
+    )
+}
---
+++ frontend/src/app/errorPage/page.tsx
@@ -0,0 +1,11 @@
+import styles from "./page.module.css"
+
+export default function Error() {
+    return(
+        <div className={styles.mainBox}>
+            <div className={styles.logo}>Articly</div>
+            <p className={styles.error}> Произошли технические шоколадки...</p>
+            <p className={styles.error}>Мы их уже съедаем ツ</p>
+        </div>
+    )
+}
