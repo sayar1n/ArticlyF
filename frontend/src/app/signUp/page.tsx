21c1e9632d85d240a93b9b0711ac12e9a797abe6
ZmoT <>
2024-10-05 14:48:27 +0300

    Добавлена оптимизация экрана Регистрации под мобильные устройства.

--- frontend/src/app/signUp/page.tsx
+++ frontend/src/app/signUp/page.tsx
@@ -1,146 +1,121 @@
 "use client";
 
-import Link from 'next/link';
-import styles from './page.module.css';
+import Link from "next/link";
+import styles from "./page.module.scss";
 import axios from "axios";
-import { useState } from "react"
-import { useRouter } from 'next/navigation';
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
-export default function SignUp(){
-    const [nickname, setNickname] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
-    const [logo, setLogo] = useState('');
-    const [error, setError] = useState<string | null>(null);
-    const router = useRouter();
+export default function SignUp() {
+  const [nickname, setNickname] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState<string | null>(null);
+  const router = useRouter();
 
-    const api = axios.create({
-        baseURL: 'http://localhost:8061',
-        withCredentials: true,
-        headers: {
-          'Content-Type': 'application/json',
-        },
-    });
+  const api = axios.create({
+    baseURL: "http://localhost:8061",
+    withCredentials: true,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  // Функция для обработки регистрации
+  const handleRegister = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      const response = await api.post("/register", {
+        nickname,
+        email,
+        password,
+      });
 
-    const handleRegister = async (e: React.FormEvent) => {
-        e.preventDefault();
-
-        if (password !== confirmPassword) {
-          setError('Пароли не совпадают.');
-          return;
+      if (response.status === 200) {
+        router.push("/signIn");
+      }
+    } catch (error: any) {
+      // Обработка ошибок API
+      if (error.response && error.response.data && error.response.data.detail) {
+        const detail = error.response.data.detail;
+        if (Array.isArray(detail)) {
+          const errorMessages = detail.map((err: any) => err.msg).join(", ");
+          setError(errorMessages);
+        } else {
+          setError(detail);
         }
+      } else {
+        setError("Ошибка регистрации.");
+      }
+      console.error("Ошибка:", error);
+    }
+  };
 
-        try {
-            const response = await api.post('/register', {
-              nickname,
-              email,
-              password,
-              logo,
-            });
-
-            if (response.status === 200) {
-              router.push('/passResetEmail');
-            }
-        } catch (error: any) {
-            if (error.response && error.response.data && error.response.data.detail) {
-              const detail = error.response.data.detail;
-              if (Array.isArray(detail)) {
-                const errorMessages = detail.map((err: any) => err.msg).join(', ');
-                setError(errorMessages);
-              } else {
-                setError(detail);
-              }
-            } else {
-              setError('Ошибка регистрации.');
-            }
-            console.error('Registration error:', error);
-        }
-    };
-
-    return (
-        <div className={styles.SignInBlock}>
+  return (
+    <div className={styles.signInBlock}>
+      <div className={styles.formSection}>
+        {/* Блок загаловка регистрации */}
+        <div className={styles.titleBlock}>
           <div className={styles.logo}>Articly</div>
-          <div className={styles.welcomeText}>
-            <p className={styles.boldHello}>Регистрация</p>
-            <span>Зарегистрируйтесь, чтобы начать пользоваться приложением</span>
-          </div>
-
-          <form onSubmit={handleRegister}>
-            <div className={styles.inputField}>
-              <div className={styles.inputNickname}>
-                <label className={styles.label}>Никнейм:</label>
-                <input
-                  type="text"
-                  className={styles.nicknameInputBox}
-                  placeholder="Введите ваш никнейм"
-                  value={nickname}
-                  onChange={(e) => setNickname(e.target.value)}
-                  required
-                />
-              </div>
-
-              <div className={styles.inputEmail}>
-                <label className={styles.label}>Почта:</label>
-                <input
-                  type="email"
-                  className={styles.emailInputBox}
-                  placeholder="email@example.com"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  required
-                />
-              </div>
-
-              <div className={styles.inputPassword}>
-                <label className={styles.label}>Пароль:</label>
-                <input
-                  type="password"
-                  className={styles.passwordInputBox}
-                  placeholder="********"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  required
-                />
-              </div>
-
-              <div className={styles.inputPassword}>
-                <label className={styles.label}>Подтверждение пароля:</label>
-                <input
-                  type="password"
-                  className={styles.passwordInputBox}
-                  placeholder="********"
-                  value={confirmPassword}
-                  onChange={(e) => setConfirmPassword(e.target.value)}
-                  required
-                />
-              </div>
-
-              <div className={styles.inputLogo}>
-                <label className={styles.label}>URL логотипа:</label>
-                <input
-                  type="text"
-                  className={styles.logoInputBox}
-                  placeholder="https://example.com/your-logo.png"
-                  value={logo}
-                  onChange={(e) => setLogo(e.target.value)}
-                />
-              </div>
-            </div>
-
-            {error && <p style={{ color: 'red' }}>{error}</p>}
-
-            <button type="submit" className={styles.signInButton}>
-              Зарегистрироваться
-            </button>
-          </form>
-
-          <div className={styles.signUpBox}>
-            <span>Есть аккаунт?</span>
-            <Link href="/signIn" className={styles.signUpText}>
-              Войти в аккаунт
-            </Link>
-          </div>
+          <span>Зарегистрируйтесь, чтобы начать пользоваться приложением</span>
         </div>
-    );
+
+        {/* Основная форма регистрации */}
+        <form onSubmit={handleRegister}>
+          {/* Поле ввода nickname */}
+          <div className={styles.inputNickname}>
+            <label className={styles.label}>Никнейм:</label>
+            <input
+              type="text"
+              className={styles.nicknameInputBox}
+              placeholder="Ваш никнейм"
+              value={nickname}
+              onChange={(e) => setNickname(e.target.value)}
+              required
+            />
+          </div>
+
+          {/* Поле ввода email */}
+          <div className={styles.inputEmail}>
+            <label className={styles.label}>Почта:</label>
+            <input
+              type="email"
+              className={styles.emailInputBox}
+              placeholder="email@example.com"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+
+          {/* Поле ввода пароля */}
+          <div className={styles.inputPassword}>
+            <label className={styles.label}>Пароль:</label>
+            <input
+              type="password"
+              className={styles.passwordInputBox}
+              placeholder="********"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+
+          {error && <p style={{ color: "red" }}>{error}</p>}
+
+          {/* Кнопка отправки формы */}
+          <button type="submit" className={styles.signInButton}>
+            Зарегистрироваться
+          </button>
+        </form>
+
+        {/* Ссылка на авторизацию */}
+        <div className={styles.signUpBox}>
+          <span>Есть аккаунт?</span>
+          <Link href="/signIn" className={styles.signUpText}>
+            Войти
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
