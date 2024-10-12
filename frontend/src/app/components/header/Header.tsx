703c8e404a51bf44c8be203b513ea0ac8894f90d
ZmoT <>
2024-10-12 21:50:30 +0300

    Добавлена адаптация под другие устройства для Header

--- frontend/src/app/components/header/Header.tsx
+++ frontend/src/app/components/header/Header.tsx
@@ -1,29 +1,57 @@
-"use client"
+"use client";
 
-import Link from 'next/link';
-import styles from './Header.module.scss';
-// import { AlignJustify, X } from 'lucide-react';
-import React, { useState} from 'react';
-
+import Link from "next/link";
+import styles from "./Header.module.scss";
+import { AlignJustify, X } from "lucide-react";
+import React, { useState } from "react";
 
 export default function Header() {
-    const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-    const toggleMenu = () => {
-        setIsOpen(!isOpen);
-    };
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
 
-    return (
-        <header className={styles.header}>
-            <div className={styles.logo}>Articly</div>
-            <nav className={styles.nav}>
-                <a href="#about" className={styles.navLink}>О нас</a>
-                <a href="#features" className={styles.navLink}>Функционал</a>
-                <a href="#pricing" className={styles.navLink}>Стоимость</a>
-                <a href="#feedback" className={styles.navLink}>Обратная связь</a>
-                <a href="#support" className={styles.navLink}>Поддержка</a>
-            </nav>
-            <Link href="/auth/signIn" className={styles.signInButton}>Войти</Link>
-        </header>
-    )
+  const navItems = [
+    { href: "#about", text: "О нас" },
+    { href: "#features", text: "Функционал" },
+    { href: "#pricing", text: "Стоимость" },
+    { href: "#feedback", text: "Обратная связь" },
+    { href: "#support", text: "Поддержка" },
+  ];
+
+  return (
+    <header>
+      <div className={styles.header}>
+        <div className={styles.logo}>Articly</div>
+        <nav className={styles.nav}>
+          {navItems.map((item) => (
+            <a key={item.href} href={item.href} className={styles.navLink}>
+              {item.text}
+            </a>
+          ))}
+        </nav>
+        <div className={styles.headerButtons}>
+          <button className={styles.hamburgerIcon} onClick={toggleMenu}>
+            {isOpen ? <X color="white" /> : <AlignJustify color="white" />}
+          </button>
+          <Link href="/auth/signIn" className={styles.signInButton}>
+            Войти
+          </Link>
+        </div>
+      </div>
+      {isOpen && (
+        <nav className={styles.navOpen}>
+          {navItems.map((item) => (
+            <a key={item.href} href={item.href} className={styles.navLinkOpen}>
+              {item.text}
+            </a>
+          ))}
+          <Link href="/auth/signIn" className={styles.signInButtonOpen}>
+            Войти
+          </Link>
+        </nav>
+      )}
+    </header>
+  );
 }
